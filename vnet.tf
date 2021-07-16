6d54013d48462d0221b38cd3e7dba5674f7bb6c9
Michael Kennedy <michaelarronkennedy@gmail.com>
2021-07-16 00:33:32 +0000

    Added vnet module

--- vnet.tf
+++ vnet.tf
@@ -1,5 +1,8 @@
 module "network" {
   source  = "app.terraform.io/arctiqmk2-training/network/azurerm"
   version = "3.0.1"
+
+  address_space = "192.168.168.0"
   resource_group_name = "myresourcegroup"
+  vnet_name = "gaurav"
 }
