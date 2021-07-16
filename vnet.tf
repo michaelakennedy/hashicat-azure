6cd341c87d7fdf9c9e82ac51bf0cbb1da8f09d0b
Michael Kennedy <michaelarronkennedy@gmail.com>
2021-07-15 20:37:00 -0400

    Trying again.

--- vnet.tf
+++ vnet.tf
@@ -1,8 +1,12 @@
 module "network" {
-  source  = "app.terraform.io/arctiqmk2-training/network/azurerm"
-  version = "3.0.1"
+  source              = "Azure/network/azurerm"
+  resource_group_name = azurerm_resource_group.myresourcegroup.name
+  address_space       = "10.0.0.0/16"
+  subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  subnet_names        = ["subnet1", "subnet2", "subnet3"]
 
-  address_space = "192.168.168.0"
-  resource_group_name = "myresourcegroup"
-  vnet_name = "gaurav"
+  tags = {
+    environment = "dev"
+    costcenter  = "it"
+  }
 }
